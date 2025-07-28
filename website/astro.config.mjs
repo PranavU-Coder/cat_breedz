4db5033405fc69f26255b2c2e5217563f6a847c6
PranavU-Coder <pranavu8406@gmail.com>
2025-07-28 12:48:25 +0530

    reworking on this site with a new template

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -1,15 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import starlight from '@astrojs/starlight';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  },
+  integrations: [starlight({
+      title: 'My Docs',
+      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+      sidebar: [
+          {
+              label: 'Guides',
+              items: [
+                  // Each item here is one entry in the navigation menu.
+                  { label: 'Example Guide', slug: 'guides/example' },
+              ],
+          },
+          {
+              label: 'Reference',
+              autogenerate: { directory: 'reference' },
+          },
+      ],
+      }), react()],
 
-  integrations: [react()]
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
